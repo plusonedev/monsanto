dcc7126ec59495f61f6b20e744d0068f0de2dcbd
Mike Kelly <mkelly@savantgroup.com>
2017-10-20 11:27:51 -0400

    added root redirect to admin site

--- monsanto/urls.py
+++ monsanto/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^preseeds/', include('preseed.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='/admin')),
 ]
