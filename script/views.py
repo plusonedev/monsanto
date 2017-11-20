47fe0c77fc9286b67c0b07f778b9fdbb26ee7349
Mike Kelly <mike@plusonedevelopments.com>
2017-11-20 03:23:49 +0000

    fixed hang up in post_install script, apps now a comma delimited string

--- script/views.py
+++ script/views.py
@@ -6,4 +6,7 @@ from preseed.models import PreseedInstance
 
 def detail(request, instance_id):
     instance = get_object_or_404(PreseedInstance, instance_id=instance_id.upper())
+
+    instance.apps = [x.strip() for x in instance.apps.split(',')]
+    
     return render(request, 'post_install', {'instance': instance}, content_type="text/plain")
